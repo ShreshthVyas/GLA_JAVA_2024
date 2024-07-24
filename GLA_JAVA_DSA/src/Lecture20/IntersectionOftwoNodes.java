6f8a26582d4661db14a8d72ae49ed7999bbcf2b1
SHRESHTH VYAS <shreshth.vyas.2000@gmail.com>
2024-07-24 22:08:40 +0530

    LinkedList -1

--- GLA_JAVA_DSA/src/Lecture20/IntersectionOftwoNodes.java
+++ GLA_JAVA_DSA/src/Lecture20/IntersectionOftwoNodes.java
@@ -1,5 +1,49 @@
 package Lecture20;
 
-public class IntersectionOftwoNodes {
+import Lecture20.MiddleOfLinkedList.ListNode;
 
+public class IntersectionOftwoNodes {
+	
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+	
+	public class Solution {
+	    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+	        ListNode a = headA;
+	        ListNode b =headB;
+	        
+	        while(a!=b) {
+	        	if(a == null) {
+	        		a = headB;
+	        	}
+	        	else {
+	        		a = a.next;
+	        	}
+	        	
+	        	if(b == null) {
+	        		b = headA;
+	        	}
+	        	else {
+	        		b = b.next;
+	        	}
+	        }
+	        
+	        return a;
+	    }
+	}
+	
 }
